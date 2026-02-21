16dad0c930174b1bb4d599dd21a9b5b38d886aa8
johnferrari-oss <jayfun072002@gmail.com>
2026-02-21 11:43:08 +0800

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using InventoryApp.Views;
+using InventoryApp.Services;
+
+class Program
+{
+    static void Main()
+    {
+        InventoryView view = new InventoryView();
+        view.Run();
+    }
+}
